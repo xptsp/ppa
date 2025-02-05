76a7f881f0e3aa9fa67920df4535fbed6d683731
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2025-02-05 09:10:04 -0600

    Added urserver-yuzu v1.0-1

---
+++ src/urserver-yuzu_1.0-1_all/opt/urserver/remotes/Unified/Main/Yuzu/remote.lua
@@ -0,0 +1,28 @@
+local keyboard = libs.keyboard;
+
+--@help Put system in sleep state
+actions.sleep = function ()
+	os.execute("systemctl suspend -i")
+	os.execute('dbus-send --system --print-reply --dest="org.freedesktop.UPower" /org/freedesktop/UPower org.freedesktop.UPower.Suspend');
+end
+
+actions.start = function()
+	os.execute("/usr/bin/urserver-yuzu-helper start");
+end
+
+actions.close = function()
+	os.execute("/usr/bin/urserver-yuzu-helper close");
+end
+
+actions.toggle = function()
+        keyboard.stroke("f11");
+end
+
+actions.stop = function()
+        keyboard.stroke("f5");
+end
+
+actions.yes = function()
+        keyboard.stroke("y");
+end
+
