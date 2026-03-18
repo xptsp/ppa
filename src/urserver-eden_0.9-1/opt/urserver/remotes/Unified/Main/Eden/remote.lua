0b8f3adddec627c323684ec1d356ac7c34072e8e
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2026-03-18 08:51:21 -0500

    Added urserver-eden v1.0-1

---
+++ src/urserver-eden_0.9-1/opt/urserver/remotes/Unified/Main/Eden/remote.lua
@@ -0,0 +1,35 @@
+local keyboard = libs.keyboard;
+
+--@help Put system in sleep state
+actions.sleep = function ()
+	os.execute("systemctl suspend -i")
+	os.execute('dbus-send --system --print-reply --dest="org.freedesktop.UPower" /org/freedesktop/UPower org.freedesktop.UPower.Suspend');
+end
+
+actions.start = function()
+	os.execute("/usr/bin/urserver-eden-helper start");
+end
+
+actions.close = function()
+	os.execute("/usr/bin/urserver-eden-helper close");
+end
+
+actions.toggle = function()
+        keyboard.stroke("f11");
+end
+
+actions.stop = function()
+        keyboard.stroke("f5");
+end
+
+actions.yes = function()
+        keyboard.stroke("y");
+end
+
+actions.tv = function()
+	os.execute("/usr/bin/urserver-eden-helper tv");
+end
+
+actions.speakers = function()
+	os.execute("/usr/bin/urserver-eden-helper speakers");
+end
